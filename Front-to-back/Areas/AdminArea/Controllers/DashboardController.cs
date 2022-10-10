a248ccd53a66e0244fc682b77ce9416225d4514d
Orkhansha <orkhansha@code.edu.az>
2022-10-11 01:42:34 +0400

    Added only Admin Panel

---
+++ Front-to-back/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Front_to_back.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class DashboardController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
